203a79c161e3207d32dd83e2db75cfdd093132d3
Hemanth kumar V <120030813+hemanthkumarv24@users.noreply.github.com>
2024-08-13 18:25:23 +0530

    Update data.ts

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -6,6 +6,7 @@ export const sampleProducts: Product[] = [
     slug: 'nike-slim-shirt',
     category: 'Shirts',
     image: '../images/p1.jpg',
+    
     price: 120,
     countInStock: 10,
     brand: 'Nike',
